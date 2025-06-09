64cbe46228360efb9df0618a8553e7861ca76e08
anoonimouse <96661202+anoonimouse@users.noreply.github.com>
2025-06-10 00:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ db.init_app(app)
 # Reddit OAuth config
 CLIENT_ID = 'ZohBbgx_RtMA4OLWTrKYTQ'
 CLIENT_SECRET = 'rUvR1cw5mh13MFo9iKu6_LdT6iKpgQ'
-REDIRECT_URI = 'https://vibeapp-qhaq.onrender.com//callback'
+REDIRECT_URI = 'https://vibeapp-qhaq.onrender.com/callback'
 USER_AGENT = 'VibeMatchApp/0.1'
 
 @app.route('/')
