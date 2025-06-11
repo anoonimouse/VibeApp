3c71ea923ab00dbe6e33c1e34a7f7f1ac0ab7231
anoonimouse <96661202+anoonimouse@users.noreply.github.com>
2025-06-11 23:35:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def login():
     reddit_auth_url = (
         "https://www.reddit.com/api/v1/authorize.compact?"
         f"client_id={CLIENT_ID}&response_type=code&state=random_string&"
-        f"redirect_uri={REDIRECT_URI}&duration=temporary&scope=identity"
+        f"redirect_uri={REDIRECT_URI}&duration=permanent&scope=identity"
     )
     return redirect(reddit_auth_url)
 
